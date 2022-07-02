4fb6da782acdfc755075adba4edbc810b5ee56f2
crisceri123 <criscerillano@gmail.com>
2022-07-02 15:05:09 -0300

    agregado php

--- contacto.php
+++ contacto.php
@@ -24,16 +24,16 @@
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav me-auto mb-2 mb-md-0">
                         <li class="nav-item ">
-                            <a class="nav-link py-1 px-3 px-sm-4" href="index.html">Inicio</a>
+                            <a class="nav-link py-1 px-3 px-sm-4" href="index.php">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link py-1 px-3 px-sm-4" href="sobre=mi.html">Sobre mi</a>
+                            <a class="nav-link py-1 px-3 px-sm-4" href="sobre=mi.php">Sobre mi</a>
                         </li>
                         <li class="nav-item ">
-                            <a class="nav-link py-1 px-3 px-sm-4" href="proyectos.html">Proyectos</a>
+                            <a class="nav-link py-1 px-3 px-sm-4" href="proyectos.php">Proyectos</a>
                         </li>
                         <li class="nav-item ">
-                            <a class="nav-link active py-1 px-3 px-sm-4" href="contacto.html">Contacto</a>
+                            <a class="nav-link active py-1 px-3 px-sm-4" href="contacto.php">Contacto</a>
                         </li>
                     </ul>
                     <div class="d-flex">
